baf3ef14ba1cebc1ac7fc4b1b5f490e9e5f85ff7
Venkat <venkata@venkatamutyala.com>
2025-08-29 08:09:22 +0000

    feat: adding tofu setup

--- glueops.sh
+++ glueops.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 cd harbor
+docker compose down
+docker system prune -a -f
+git clean -xdf
 source .env
 cat harbor.yml.tmpl | envsubst > harbor.yml
-./install.sh
+sudo ./install.sh
