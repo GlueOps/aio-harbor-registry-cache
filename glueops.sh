594a5ed1ac13d37662c5f0ce543ae0487dc3ae3a
Venkat <venkata@venkatamutyala.com>
2025-08-29 08:13:39 +0000

    # This is a combination of 6 commits. # This is the 1st commit message:
    fix: glueops.sh
    
    # This is the commit message #2:
    
    remove nvidia and k8s to fix later
    
    # This is the commit message #3:
    
    testing workflows
    
    # This is the commit message #4:
    
    fixes'
    
    # This is the commit message #5:
    
    fixes
    
    # This is the commit message #6:
    
    fixes

--- glueops.sh
+++ glueops.sh
@@ -2,7 +2,21 @@
 cd harbor
 docker compose down
 docker system prune -a -f
-git clean -xdf
+sudo git clean -xdf
 source .env
 cat harbor.yml.tmpl | envsubst > harbor.yml
 sudo ./install.sh
+cd ../opentofu-setup
+tofu init
+sleep 10;
+tofu plan
+tofu apply -auto-approve
+cd ..
+
+
+docker pull localhost:80/proxy-docker-io/alpine:latest
+docker pull localhost:80/proxy-docker-io/nginx/nginx-ingress:latest
+docker pull localhost:80/proxy-quay-io/argoproj/argocd:latest
+#docker pull localhost:80/istio-release/base:1.25-2025-08-28T19-03-14
+docker pull localhost:80/proxy-ghcr-io/argoproj/argocd:latest
+docker pull localhost:80/proxy-public-ecr-aws/nginx/nginx:latest
