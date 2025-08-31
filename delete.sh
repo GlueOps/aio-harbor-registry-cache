60e4816d72d14463649cc11657aa67acf0050a6b
Venkat <venkata@venkatamutyala.com>
2025-08-31 09:08:25 +0000

    fixes

--- delete.sh
+++ delete.sh
@@ -6,7 +6,7 @@ echo "THIS WILL KILL YOUR CODESPACE, IT WILL ALSO DELETE/RESET ANY GIT CHANGES!!
 sleep 10;
 
 #stop all containers except for codespace (this helps for local dev cycles)
-docker stop $(docker ps -q)
+docker stop $(docker ps -q) || true
 docker system prune -a -f
 git clean -xdf
 git reset --hard
