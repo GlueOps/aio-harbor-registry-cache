e05f38776f2a9577111a06ad76e324e0022b1a26
Venkat <venkata@venkatamutyala.com>
2025-08-31 08:48:44 +0000

    fixes

--- delete.sh
+++ delete.sh
@@ -2,7 +2,14 @@
 set -e
 
 #stop all containers except for codespace (this helps for local dev cycles)
-docker stop $(docker ps -a -q | grep -v $(docker ps -a -q --filter name="^/codespace$"))
+ID_TO_KEEP=$(docker ps -a -q --filter name="^/codespace$")
+IDS_TO_STOP=$(docker ps -a -q | grep -v "${ID_TO_KEEP}")
+if [ -n "$IDS_TO_STOP" ]; then
+  docker stop $IDS_TO_STOP
+else
+  echo "No other containers to stop."
+fi
+
 docker system prune -a -f
 git clean -xdf
 git reset --hard
