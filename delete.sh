ac9f927340fb4e43f0950fd690ee5c871e955b01
Venkat <venkata@venkatamutyala.com>
2025-08-31 17:57:44 +0000

    fixes

--- delete.sh
+++ delete.sh
@@ -6,7 +6,6 @@ echo "THIS WILL DELETE/RESET ANY GIT CHANGES!!"
 sleep 5;
 
 #stop all containers except for codespace (this helps for local dev cycles)
-cd harbor
 docker-compose down || true
 docker stop nginx-simple-redirect || true
 cd ..
