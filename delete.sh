ef404c495a77dd401bb05185327e1a39b6e6d2ca
Venkat <venkata@venkatamutyala.com>
2025-08-31 18:00:40 +0000

    fixes

--- delete.sh
+++ delete.sh
@@ -7,8 +7,8 @@ sleep 5;
 
 #stop all containers except for codespace (this helps for local dev cycles)
 cd harbor
-docker-compose down || true
-docker stop nginx-simple-redirect || true
+sudo docker-compose down || true
+sudo docker stop nginx-simple-redirect || true
 cd ..
 docker system prune -a -f
 sudo git clean -xdf
