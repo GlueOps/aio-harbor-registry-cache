f51396d079bbf65c5a2201be97bdb6c28a1598ed
Venkat <venkata@venkatamutyala.com>
2025-09-03 00:26:42 +0000

    testing

--- opentofu-setup/proxied-registries.tf
+++ opentofu-setup/proxied-registries.tf
@@ -3,7 +3,7 @@ module "dockerhub_proxy" {
   source = "./modules/proxy-project"
 
   project_name          = "proxy-docker-io"
-  registry_provider     = var.harbor_registry_mode == "REPLICA" ? "docker-registry" : "docker-hub"
+  registry_provider     = var.harbor_registry_mode == "REPLICA" ? "docker-registry" : "docker-registry"
   registry_endpoint_url = var.harbor_registry_mode == "REPLICA" ? "https://${var.harbor_core_hostname}/proxy-docker-io" : "https://hub.docker.com"
   admin_group_name      = local.admin_group_name
 }
@@ -13,7 +13,7 @@ module "quay_proxy" {
   source = "./modules/proxy-project"
 
   project_name          = "proxy-quay-io"
-  registry_provider     = var.harbor_registry_mode == "REPLICA" ? "docker-registry" : "quay"
+  registry_provider     = var.harbor_registry_mode == "REPLICA" ? "docker-registry" : "docker-registry"
   registry_endpoint_url = var.harbor_registry_mode == "REPLICA" ? "https://${var.harbor_core_hostname}/proxy-quay-io" : "https://quay.io"
   admin_group_name      = local.admin_group_name
 
@@ -35,7 +35,7 @@ module "ghcr_proxy" {
   source = "./modules/proxy-project"
 
   project_name          = "proxy-ghcr-io"
-  registry_provider     = var.harbor_registry_mode == "REPLICA" ? "docker-registry" : "docker-hub"
+  registry_provider     = var.harbor_registry_mode == "REPLICA" ? "docker-registry" : "docker-registry"
   registry_endpoint_url = var.harbor_registry_mode == "REPLICA" ? "https://${var.harbor_core_hostname}/proxy-ghcr-io" : "https://ghcr.io"
   admin_group_name      = local.admin_group_name
 
